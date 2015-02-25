248b0397c10d29162524ad34e51da6c8f5a276cc
wea_ondara <wea_ondara@alpenblock.net>
2015-02-25 21:26:39 +0100

    cleanup

--- src/main/java/net/alpenblock/bungeeperms/bukkit/io/mysql2/ValueEntry.java
+++ src/main/java/net/alpenblock/bungeeperms/bukkit/io/mysql2/ValueEntry.java
@@ -1,39 +1,16 @@
 package net.alpenblock.bungeeperms.bukkit.io.mysql2;
 
-public class ValueEntry 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ValueEntry
 {
+
     private String value;
     private String server;
     private String world;
-
-    public ValueEntry(String value, String server, String world) {
-        this.value = value;
-        this.server = server;
-        this.world = world;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    public String getServer() {
-        return server;
-    }
-
-    public void setServer(String server) {
-        this.server = server;
-    }
-
-    public String getWorld() {
-        return world;
-    }
-
-    public void setWorld(String world) {
-        this.world = world;
-    }
-    
 }
