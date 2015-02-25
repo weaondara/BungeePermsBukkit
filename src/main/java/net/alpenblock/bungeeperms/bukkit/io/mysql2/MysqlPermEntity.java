ba87d310146fd0d287f9e8789592596316b91666
wea_ondara <wea_ondara@alpenblock.net>
2015-02-25 21:37:03 +0100

    fix sql injection

--- src/main/java/net/alpenblock/bungeeperms/bukkit/io/mysql2/MysqlPermEntity.java
+++ src/main/java/net/alpenblock/bungeeperms/bukkit/io/mysql2/MysqlPermEntity.java
@@ -6,75 +6,60 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import lombok.Getter;
+import net.alpenblock.bungeeperms.bukkit.Mysql;
 
+@Getter
 public class MysqlPermEntity 
 {
+    
     private String name;
     private EntityType type;
-    private Map<String,List<ValueEntry>> data;
-    
-    
+    private Map<String, List<ValueEntry>> data;
+
     public MysqlPermEntity(ResultSet res) throws SQLException
     {
-        data=new HashMap<>();
+        data = new HashMap<>();
         load(res);
     }
-    
+
     private void load(ResultSet res) throws SQLException
     {
-        if(res.first())
+        if (res.first())
         {
-            name=res.getString("name");
-            type=EntityType.getByCode(res.getInt("type"));
+            name = Mysql.unescape(res.getString("name"));
+            type = EntityType.getByCode(res.getInt("type"));
         }
-        
+
         res.beforeFirst();
-        
-        while(res.next())
+
+        while (res.next())
         {
-            String key=res.getString("key");
-            String value=res.getString("value");
-            String server=res.getString("server");
-            String world=null;
-            if(res.wasNull())
+            String key = Mysql.unescape(res.getString("key"));
+            String value = Mysql.unescape(res.getString("value"));
+            String server = Mysql.unescape(res.getString("server"));
+            String world = null;
+            if (server != null)
             {
-                server=null;
+                world = Mysql.unescape(res.getString("world"));
             }
-            else
-            {
-                world=res.getString("world");
-                if(res.wasNull())
-                {
-                    world=null;
-                }
-            }
-            
+
             //add entry
-            ValueEntry ve=new ValueEntry(value,server,world);
-            
+            ValueEntry ve = new ValueEntry(value, server, world);
+
             List<ValueEntry> e = data.get(key);
-            if(e==null)
+            if (e == null)
             {
-                e=new ArrayList<>();
+                e = new ArrayList<>();
                 data.put(key, e);
             }
-            
+
             e.add(ve);
         }
+        
+        //todo: close res?
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public EntityType getType() {
-        return type;
-    }
-
-    public Map<String,List<ValueEntry>> getAllData() {
-        return data;
-    }
-    
     public List<ValueEntry> getData(String type)
     {
         return data.get(type);
